354a1e14b5c56828a0e7e85d3cd107bdc0d0915b
amane@ProximaMidnight <amane.dev@gmail.com>
2024-03-30 21:20:56 +0000

    deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'code',
+    title: 'Code de la Route',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
