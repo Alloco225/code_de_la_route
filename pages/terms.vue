367250340311953fdf72a985bbff9c7fe48ae9c0
amane@ProximaMidnight <amane.dev@gmail.com>
2024-03-30 19:30:42 +0000

    📖 DOC: update

--- pages/terms.vue
+++ pages/terms.vue
@@ -2,11 +2,11 @@
   div(class="bg-gray-100")
     div(class="container mx-auto p-8")
       h1(class="text-2xl font-bold mb-4") Conditions générales d'utilisation de l'application
-      p Date d'entrée en vigueur : [insérer la date d'entrée en vigueur]
-      p Bienvenue sur l'application "Apprendre les règles de conduite". Veuillez lire attentivement les présentes conditions générales d'utilisation ("Conditions") avant d'utiliser cette application.
-      p En utilisant l'application "Apprendre les règles de conduite", vous acceptez d'être lié par les présentes Conditions. Si vous n'acceptez pas toutes les conditions de cet accord, vous ne pouvez pas utiliser l'application.
+      p Date d'entrée en vigueur : 01/03/2024
+      p Bienvenue sur l'application "Code de la Route". Veuillez lire attentivement les présentes conditions générales d'utilisation ("Conditions") avant d'utiliser cette application.
+      p En utilisant l'application "Code de la Route", vous acceptez d'être lié par les présentes Conditions. Si vous n'acceptez pas toutes les conditions de cet accord, vous ne pouvez pas utiliser l'application.
       h2(class="text-xl font-bold mt-4 mb-2") Utilisation de l'application
-      p L'application "Apprendre les règles de conduite" est destinée à être utilisée dans le but d'apprendre et de réviser les règles de conduite et les signaux routiers.
+      p L'application "Code de la Route" est destinée à être utilisée dans le but d'apprendre et de réviser les règles de conduite et les signaux routiers.
       p Vous acceptez de n'utiliser l'application que de manière légale et conforme aux présentes Conditions.
       h2(class="text-xl font-bold mt-4 mb-2") Compte utilisateur
       p Pour utiliser certaines fonctionnalités de l'application, vous pouvez être tenu de créer un compte utilisateur.
@@ -22,7 +22,7 @@
       p Ces informations sont collectées dans le but d'améliorer l'expérience utilisateur et de fournir des fonctionnalités telles qu'un suivi de la progression et un classement public.
       p Nous nous engageons à ne pas partager vos informations personnelles avec des tiers sans votre consentement explicite.
       h2(class="text-xl font-bold mt-4 mb-2") Propriété intellectuelle
-      p Tous les droits de propriété intellectuelle relatifs à l'application et à son contenu restent la propriété de [nom de votre entreprise] ou de ses concédants de licence.
+      p Tous les droits de propriété intellectuelle relatifs à l'application et à son contenu restent la propriété de Amane.dev ou de ses concédants de licence.
       h2(class="text-xl font-bold mt-4 mb-2") Modification des conditions
       p Nous nous réservons le droit de modifier ces Conditions à tout moment. Les modifications prendront effet dès leur publication sur l'application.
       h2(class="text-xl font-bold mt-4 mb-2") Résiliation
@@ -30,8 +30,8 @@
       h2(class="text-xl font-bold mt-4 mb-2") Limitation de responsabilité
       p Dans toute la mesure permise par la loi applicable, nous déclinons toute responsabilité pour tout dommage direct, indirect, accessoire, spécial, consécutif ou exemplaire résultant de votre utilisation de l'application.
       h2(class="text-xl font-bold mt-4 mb-2") Lois applicables
-      p Ces Conditions sont régies et interprétées conformément aux lois de [insérer le pays ou la juridiction pertinente].
-      p En utilisant l'application "Apprendre les règles de conduite", vous acceptez d'être lié par ces Conditions. Si vous avez des questions ou des préoccupations concernant ces Conditions, veuillez nous contacter à [insérer une adresse e-mail de contact].
+      p Ces Conditions sont régies et interprétées conformément aux lois de la République de Côte d'Ivoire.
+      p En utilisant l'application "Code de la Route", vous acceptez d'être lié par ces Conditions. Si vous avez des questions ou des préoccupations concernant ces Conditions, veuillez nous contacter à hello@amane.dev.
 </template>
 
 <script>
