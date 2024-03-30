a33be7cfb9b81451f6075d7a9619fe03cfb948c2
amane@ProximaMidnight <amane.dev@gmail.com>
2024-03-30 19:27:48 +0000

    📖 DOC: terms

---
+++ pages/terms.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+  div(class="bg-gray-100")
+    div(class="container mx-auto p-8")
+      h1(class="text-2xl font-bold mb-4") Conditions générales d'utilisation de l'application
+      p Date d'entrée en vigueur : [insérer la date d'entrée en vigueur]
+      p Bienvenue sur l'application "Apprendre les règles de conduite". Veuillez lire attentivement les présentes conditions générales d'utilisation ("Conditions") avant d'utiliser cette application.
+      p En utilisant l'application "Apprendre les règles de conduite", vous acceptez d'être lié par les présentes Conditions. Si vous n'acceptez pas toutes les conditions de cet accord, vous ne pouvez pas utiliser l'application.
+      h2(class="text-xl font-bold mt-4 mb-2") Utilisation de l'application
+      p L'application "Apprendre les règles de conduite" est destinée à être utilisée dans le but d'apprendre et de réviser les règles de conduite et les signaux routiers.
+      p Vous acceptez de n'utiliser l'application que de manière légale et conforme aux présentes Conditions.
+      h2(class="text-xl font-bold mt-4 mb-2") Compte utilisateur
+      p Pour utiliser certaines fonctionnalités de l'application, vous pouvez être tenu de créer un compte utilisateur.
+      p Vous êtes responsable de maintenir la confidentialité de votre mot de passe et de votre compte, et vous êtes entièrement responsable de toutes les activités qui se produisent sous votre compte.
+      h2(class="text-xl font-bold mt-4 mb-2") Collecte et utilisation des données
+      p L'application collecte et stocke les informations suivantes :
+      ul(class="list-disc ml-8")
+        li Nom d'utilisateur
+        li Image de profil (facultative)
+        li Score de quiz
+        li Historique des quiz
+        li Possiblement, un classement public
+      p Ces informations sont collectées dans le but d'améliorer l'expérience utilisateur et de fournir des fonctionnalités telles qu'un suivi de la progression et un classement public.
+      p Nous nous engageons à ne pas partager vos informations personnelles avec des tiers sans votre consentement explicite.
+      h2(class="text-xl font-bold mt-4 mb-2") Propriété intellectuelle
+      p Tous les droits de propriété intellectuelle relatifs à l'application et à son contenu restent la propriété de [nom de votre entreprise] ou de ses concédants de licence.
+      h2(class="text-xl font-bold mt-4 mb-2") Modification des conditions
+      p Nous nous réservons le droit de modifier ces Conditions à tout moment. Les modifications prendront effet dès leur publication sur l'application.
+      h2(class="text-xl font-bold mt-4 mb-2") Résiliation
+      p Nous nous réservons le droit de résilier votre accès à l'application à tout moment, sans préavis ni responsabilité, pour quelque raison que ce soit.
+      h2(class="text-xl font-bold mt-4 mb-2") Limitation de responsabilité
+      p Dans toute la mesure permise par la loi applicable, nous déclinons toute responsabilité pour tout dommage direct, indirect, accessoire, spécial, consécutif ou exemplaire résultant de votre utilisation de l'application.
+      h2(class="text-xl font-bold mt-4 mb-2") Lois applicables
+      p Ces Conditions sont régies et interprétées conformément aux lois de [insérer le pays ou la juridiction pertinente].
+      p En utilisant l'application "Apprendre les règles de conduite", vous acceptez d'être lié par ces Conditions. Si vous avez des questions ou des préoccupations concernant ces Conditions, veuillez nous contacter à [insérer une adresse e-mail de contact].
+</template>
+
+<script>
+export default {
+  name: "Terms",
+};
+</script>
+
+<style></style>
--- utils/utils.js
+++
@@ -1,7 +0,0 @@
-const sleep = function (millisecondsDuration) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, millisecondsDuration);
-  });
-};
-
-export default sleep;
